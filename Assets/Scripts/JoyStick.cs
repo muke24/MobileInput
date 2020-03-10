4480623ea48276685b128abf087c3dc869f712ad
Peter <pjthompson.au@gmail.com>
2020-03-10 12:33:12 +1100

    JoySticks

---
+++ Assets/Scripts/JoyStick.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class JoyStick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    #region Serialized Fields
+    [SerializeField]
+    private float handlerRange = 1;
+    [SerializeField]
+    private float deadZone = 0;
+    [SerializeField]
+    private AxisOptions axisOptions = AxisOptions.Both;
+    [SerializeField]
+    private bool snapX = false;
+    [SerializeField]
+    private bool snapY = false;
+    [SerializeField]
+    protected RectTransform background;
+    [SerializeField]
+    protected RectTransform handle;
+    #endregion
+    #region Variables
+    private RectTransform baseRect;
+    private Canvas canvas;
+    private Camera cam;
+    public Vector2 input = Vector2.zero;
+    #endregion
+    #region Properities
+    private float SnapFloat(float value, AxisOptions snapAxis)
+    {
+        if (value == 0)
+        {
+            return 0;
+        }
+        if (axisOptions == AxisOptions.Both)
+        {
+            float angle = Vector2.Angle(input, Vector2.up);
+            if (snapAxis == AxisOptions.Horizontal)
+            {
+                if (angle < 22.5f || angle > 157.5f)
+                    return 0;
+                else
+                    return (value > 0) ? 1 : -1;
+            }
+            else if (snapAxis == AxisOptions.Vertical)
+            {
+                if (angle > 67.5f && angle < 112.5f)
+                    return 0;
+                else
+                    return (value > 0) ? 1 : -1;
+            }
+            else
+            {
+                if (value > 0)
+                    return 1;
+                if (value < 0)
+                    return -1;
+            }
+        }
+        return 0;
+    }
+    public bool SnapX
+    {
+        get { return snapX; }
+        set { snapX = value; }
+    }
+    public bool SnapY
+    {
+        get { return snapY; }
+        set { snapY = value; }
+    }
+    public float HandleRange
+    {
+        get { return deadZone; }
+        set { deadZone = Mathf.Abs(value); }
+    }
+    public AxisOptions AxisOption
+    {
+        get { return axisOptions; }
+        set { axisOptions = value; }
+    }
+    public float Horizontal
+    {
+        get { return (snapX) ? SnapFloat(input.x, AxisOptions.Horizontal) : input.y; }
+    }
+    public float Vertical
+    {
+        get { return (snapY) ? SnapFloat(input.y, AxisOptions.Vertical) : input.x; }
+    }
+    public Vector2 Direction
+    {
+        get { return new Vector2(Horizontal, Vertical); }
+    }
+    #endregion
+    protected virtual void Start()
+    {
+        baseRect = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+        if (canvas == null)
+        {
+            Debug.LogError("The Joystick is not placed inside the Canvas! Der Scrub");
+        }
+        Vector2 center = new Vector2(0.5f, 0.5f);
+        background.pivot = center;
+        handle.anchorMin = center;
+        handle.anchorMax = center;
+        handle.pivot = center;
+        handle.anchoredPosition = Vector2.zero;
+
+    }
+    private void FormatInput()
+    {
+        if (axisOptions == AxisOptions.Horizontal)
+        {
+            input = new Vector2(input.x, 0f);
+        }
+        else if (AxisOption == AxisOptions.Vertical)
+        {
+            input = new Vector2(0f, input.y);
+        }
+    }
+    protected virtual void HandleInput(float magnitude, Vector2 normalised, Vector2 radius, Camera cam)
+    {
+        if (magnitude > deadZone)
+        {
+            if (magnitude > 1)
+            {
+                input = normalised;
+            }
+            else
+            {
+                input = Vector2.zero;
+            }
+        }
+    }
+    protected Vector2 ScreenPointToAnchorPosition(Vector2 screenPosition)
+    {
+        Vector2 localPoint = Vector2.zero;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(baseRect, screenPosition, cam, out localPoint))
+        {
+            Vector2 pivotOffset = baseRect.pivot * baseRect.sizeDelta;
+            return localPoint - (background.anchorMax * baseRect.sizeDelta) + pivotOffset;
+        }
+        return Vector2.zero;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        cam = null;
+        if (canvas.renderMode == RenderMode.ScreenSpaceCamera)
+        {
+            cam = canvas.worldCamera;
+        }
+        Vector2 position = RectTransformUtility.WorldToScreenPoint(cam, background.position);
+        Vector2 radius = background.sizeDelta / 2;
+        input = (eventData.position - position) / (radius * canvas.scaleFactor);
+        FormatInput();
+        HandleInput(input.magnitude, input.normalized, radius, cam);
+        handle.anchoredPosition = input * radius * handlerRange;
+    }
+
+    public virtual void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+
+    public virtual void OnPointerUp(PointerEventData eventData)
+    {
+        input = Vector2.zero;
+        handle.anchoredPosition = Vector2.zero;
+    }
+}
+public enum AxisOptions
+{
+    Both, Horizontal, Vertical
+}
---
+++ Assets/Scripts/VariableJoystick.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class VariableJoystick : JoyStick
+{
+    public float MoveThreshold
+    {
+        get
+        {
+            return MoveThreshold;
+        }
+        set
+        {
+            MoveThreshold = Mathf.Abs(value);
+        }
+        
+    }
+    [SerializeField]
+    private float moveThreshold = 1;
+    [SerializeField]
+    private JoyStickType joyStickType = JoyStickType.Fixed;
+    private Vector2 fixedPosition = Vector2.zero;
+
+    public void SetMode(JoyStickType joyStickType)
+    {
+        this.joyStickType = joyStickType;
+        if (joyStickType == JoyStickType.Fixed)
+        {
+            background.anchoredPosition = fixedPosition;
+            background.gameObject.SetActive(true);
+        }
+        else
+        {
+            background.gameObject.SetActive(false);
+        }
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        fixedPosition = background.anchoredPosition;
+        SetMode(joyStickType);
+    }
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        background.gameObject.SetActive(true);
+
+        if (joyStickType != JoyStickType.Fixed)
+        {
+
+            background.anchoredPosition = ScreenPointToAnchorPosition(eventData.position);
+        }
+
+        base.OnPointerDown(eventData);
+
+    }
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        if (joyStickType != JoyStickType.Fixed)
+        {
+            background.gameObject.SetActive(false);
+        }
+        base.OnPointerUp(eventData);
+    }
+    protected override void HandleInput(float magnitude, Vector2 normalised, Vector2 radius, Camera cam)
+    {
+        if (joyStickType == JoyStickType.Dynamic && magnitude > moveThreshold)
+        {
+            Vector2 difference = normalised * (magnitude - moveThreshold) * radius;
+            background.anchoredPosition += difference;
+        }
+        base.HandleInput(magnitude, normalised, radius, cam);
+    }
+}
+
+public enum JoyStickType
+{
+    Fixed, Floating, Dynamic
+}
